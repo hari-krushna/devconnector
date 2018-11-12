5f14eeb668307470b95cf06a5341863fe442a502
hari-krushna <harikrushna2009@gmail.com>
2018-11-12 14:05:19 +0530

    added route get profile by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,18 +37,35 @@ router.get(
 
 // @route GET api/profile/handle/:handle
 // @desc get profile by handle
-// access public
+// @access public
 router.get("/handle/:handle", (req, res) => {
   const errors = {};
   Profile.findOne({ handle: req.params.handle })
-  .populate("user", [ "name", "avatar"])
-  .then(profile => {
-      if(!profile) {
-          errors.noprofile = "No profile exists for the handle";
-          res.status(404).json(errors)
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "No profile exists for the handle";
+        res.status(404).json(errors);
       }
 
-      res.json(profile)
+      res.json(profile);
+    })
+    .catch(error => res.json({ profile: "There is no profile for this user"}));
+});
+
+// @route GET api/profile/user/:user_id
+// @desc get profile by user ID
+// @access public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+  .populate('user', ["name", "avatar"])
+  .then(profile => {
+    if (!profile) {
+      errors.noprofile = "There is no profile for this user"
+      res.status(404).json(errors)
+    }
+    res.json(profile)
   })
   .catch(error => res.json(error))
 });
