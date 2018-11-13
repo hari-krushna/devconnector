e992c325d219a45db467b86eb6318a7036ebd9bb
hari-krushna <harikrushna2009@gmail.com>
2018-11-13 16:11:14 +0530

    get posts route added

---
+++ validators/post.js
@@ -0,0 +1,22 @@
+const validator = require("validator");
+const isEmpty = require("./is_empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  // Validators for Text field
+  if (!validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Text field should be between 10 and 300 characters";
+  }
+
+  if (validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
